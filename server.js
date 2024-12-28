f734b3dc32d15f4e5e643c5457cbbb4dcc1e0694
abolfazl jazak <jazakabolfazl@gmail.com>
2024-12-28 18:11:30 +0330

    refactor(server): add clean code and formater

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-const express = require("express")
-const app = express()
-const config = require("./config")
-const users = require("./users")
+const express = require("express");
+const app = express();
+const config = require("./config");
+const users = require("./users");
 
-app.use(express.static(__dirname + "/public"))
-app.use(express.urlencoded({extended: false}))
-app.get('/', (req, res) => {
-    res.send(`hello ${req.query.username}`)
-})
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  res.send(`hello ${req.query.username}`);
+});
 
-app.get('/user', (req, res) => {
-    res.status(200).json({
-        data: users,
-        success: true
-    })
-})
+app.get("/user", (req, res) => {
+  res.status(200).json({
+    data: users,
+    success: true,
+  });
+});
 
-app.get('/user/:id', (req, res) => {
-    let user = users.find(user => {
-        if (user.id == req.params.id)
-            return user
-    })
-    res.status(200).json({
-        data: user,
-        success: true
-    })
-})
+app.get("/user/:id", (req, res) => {
+  let user = users.find((user) => {
+    if (user.id == req.params.id) return user;
+  });
+  res.status(200).json({
+    data: user,
+    success: true,
+  });
+});
 
-app.post('/user', (req, res) => {
-    const user = req.body
-    user.id = parseInt(user.id)
-    users.push(user)
-})
+app.post("/user", (req, res) => {
+  const user = req.body;
+  user.id = parseInt(user.id);
+  users.push(user);
+  res.json({
+    data: "کاربر با موفقیت اضافه شد.",
+    success: true,
+  });
+});
 
 app.listen(config.port, () => {
-    console.log(`server is running on port ${config.port}`)
-})
+  console.log(`server is running on port ${config.port}`);
+});
