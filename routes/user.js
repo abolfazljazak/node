2f0be82f25967fe9efcaa1c78618a9861479db9c
abolfazl jazak <jazakabolfazl@gmail.com>
2024-12-30 02:35:07 +0330

    feat(routers): add folder routers and add rout user and delete rout user in server

---
+++ routes/user.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const router = express.Router();
+const users = require("../users");
+const { check, validationResult } = require("express-validator");
+
+router.get("/user", (req, res) => {
+  res.status(200).json({
+    data: users,
+    success: true,
+  });
+});
+
+router.get("/user/:id", (req, res) => {
+  let user = users.find((user) => {
+    if (user.id == req.params.id) return user;
+  });
+  res.status(200).json({
+    data: user,
+    success: true,
+  });
+});
+
+router.post(
+  "/user",
+  [check("email").isEmail(), check("password").isLength({ min: 5 })],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array(),
+      });
+    }
+    const user = req.body;
+    user.id = parseInt(user.id);
+    users.push(user);
+    res.json({
+      data: "کاربر با موفقیت اضافه شد.",
+      success: true,
+    });
+  }
+);
+
+router.put("/user/:id", (req, res) => {
+  users = users.map((user) => {
+    if (user.id == req.params.id) {
+      return req.body;
+    } else {
+      return user;
+    }
+  });
+  res.json({
+    data: "کاربر با موفقیت بروز شد.",
+    success: true,
+  });
+});
+
+router.delete("/user/:id", (req, res) => {
+  user = users.find((user) => {
+    if (user.id !== req.params.id) {
+    }
+  });
+  res.json({
+    data: "کاربر با موفقیت حذف شد.",
+    success: true,
+  });
+});
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 const config = require("./config");
-const users = require("./users");
-const { check, validationResult } = require("express-validator");
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: false }));
@@ -10,67 +8,7 @@ app.get("/", (req, res) => {
   res.send(`hello ${req.query.username}`);
 });
 
-app.get("/user", (req, res) => {
-  res.status(200).json({
-    data: users,
-    success: true,
-  });
-});
-
-app.get("/user/:id", (req, res) => {
-  let user = users.find((user) => {
-    if (user.id == req.params.id) return user;
-  });
-  res.status(200).json({
-    data: user,
-    success: true,
-  });
-});
-
-app.post(
-  "/user",
-  [check("email").isEmail(), check("password").isLength({ min: 5 })],
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({
-        errors: errors.array(),
-      });
-    }
-    const user = req.body;
-    user.id = parseInt(user.id);
-    users.push(user);
-    res.json({
-      data: "کاربر با موفقیت اضافه شد.",
-      success: true,
-    });
-  }
-);
-
-app.put("/user/:id", (req, res) => {
-  users = users.map((user) => {
-    if (user.id == req.params.id) {
-      return req.body;
-    } else {
-      return user;
-    }
-  });
-  res.json({
-    data: "کاربر با موفقیت اضافه شد.",
-    success: true,
-  });
-});
-
-app.delete("/user/:id", (req, res) => {
-  user = users.find((user) => {
-    if (user.id !== req.params.id) {
-    }
-  });
-  res.json({
-    data: "کاربر با موفقیت اضافه شد.",
-    success: true,
-  });
-});
+app.use('/user', require("./routes/user"))
 
 app.listen(config.port, () => {
   console.log(`server is running on port ${config.port}`);
